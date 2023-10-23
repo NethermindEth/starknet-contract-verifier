9bcc04e661b57523e206f67f0575118a26aa2cad
A5Farmer <95952749+hypekn1ght@users.noreply.github.com>
2023-10-23 21:20:56 +0800

    added 2.2.0/0.7.0 in utils.rs and typo fix on v2_2_0

--- crates/resolver-cli/src/resolver.rs
+++ crates/resolver-cli/src/resolver.rs
@@ -4,7 +4,7 @@ use dyn_compiler::dyn_compiler::{DynamicCompiler, SupportedCairoVersions};
 
 pub fn get_dynamic_compiler(cairo_version: SupportedCairoVersions) -> Box<dyn DynamicCompiler> {
     match cairo_version {
-        SupportedCairoVersions::V2_0_2 => Box::new(VoyagerGeneratorV2_2_0),
+        SupportedCairoVersions::V2_2_0 => Box::new(VoyagerGeneratorV2_2_0),
         _ => todo!()
     }
 }
