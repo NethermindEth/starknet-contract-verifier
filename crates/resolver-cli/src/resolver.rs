3711dc8110b593ce2f7e4bd1e92cab20565d8614
julio4 <jules.doumeche@gmail.com>
2024-05-02 20:02:14 +0900

    fix: rename generator

--- crates/resolver-cli/src/resolver.rs
+++ crates/resolver-cli/src/resolver.rs
@@ -1,10 +1,10 @@
-use voyager_resolver_cairo_2_6_3::dyn_compiler::VoyagerGeneratorWrapper as VoyagerGeneratorV2_4_3;
+use voyager_resolver_cairo_2_6_3::dyn_compiler::VoyagerGeneratorWrapper as VoyagerGeneratorV2_6_3;
 
 use dyn_compiler::dyn_compiler::{DynamicCompiler, SupportedCairoVersions};
 
 pub fn get_dynamic_compiler(cairo_version: SupportedCairoVersions) -> Box<dyn DynamicCompiler> {
     match cairo_version {
-        SupportedCairoVersions::V2_6_3 => Box::new(VoyagerGeneratorV2_4_3),
+        SupportedCairoVersions::V2_6_3 => Box::new(VoyagerGeneratorV2_6_3),
         // _ => todo!(),
     }
 }
