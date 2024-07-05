9b4014588f46c016913a1a24f8fbcd694a2df1be
liam <liam@nethermind.io>
2024-07-05 08:30:25 -0400

    clean

--- crates/cli/src/cli.rs
+++ crates/cli/src/cli.rs
@@ -10,11 +10,12 @@ use crate::utils::detect_local_tools;
 use camino::Utf8PathBuf;
 use console::{style, Emoji};
 use dialoguer::{theme::ColorfulTheme, Confirm, Input, Select};
+use dirs::home_dir;
 use indicatif::{HumanDuration, ProgressStyle};
 use regex::Regex;
 use std::{env, time::Instant};
 use strum::IntoEnumIterator;
-use validation::{expand_tilde, is_class_hash_valid};
+use validation::is_class_hash_valid;
 use verify::VerifyProjectArgs;
 
 #[allow(dead_code)]
@@ -57,7 +58,14 @@ fn main() -> anyhow::Result<()> {
             .trim()
             .to_string()
     };
-    let utf8_path: Utf8PathBuf = expand_tilde(&path);
+    // Resolve path
+    let mut utf8_path = Utf8PathBuf::from(&path);
+    if utf8_path.starts_with("~") {
+        if let Some(home) = home_dir() {
+            let home_utf8 = Utf8PathBuf::from_path_buf(home).unwrap();
+            utf8_path = home_utf8.join(utf8_path.strip_prefix("~").unwrap());
+        }
+    }
     if !utf8_path.exists() {
         panic!("Path does not exist");
     }
--- crates/cli/src/validation.rs
+++ crates/cli/src/validation.rs
@@ -1,5 +1,3 @@
-use camino::Utf8PathBuf;
-use dirs::home_dir;
 use regex::Regex;
 
 const NORMALIZED_HASH_LENGTH: usize = 66;
@@ -13,19 +11,6 @@ pub fn is_class_hash_valid(hash: &str) -> bool {
     }
 
     false
-}
-
-pub fn expand_tilde(path: &String) -> Utf8PathBuf {
-    let path = Utf8PathBuf::from(path);
-
-    if path.starts_with("~") {
-        if let Some(home) = home_dir() {
-            let home_utf8 = Utf8PathBuf::from_path_buf(home).unwrap();
-            return home_utf8.join(path.strip_prefix("~").unwrap());
-        }
-    }
-
-    path
 }
 
 #[cfg(test)]
@@ -54,15 +39,5 @@ mod tests {
     fn test_invalid_class_hash_no_prefix() {
         let invalid_hash = "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef";
         assert!(!is_class_hash_valid(invalid_hash));
-    }
-
-    #[test]
-    fn test_expand_tilde() {
-        let path = String::from("~/cairo_2_4_3");
-        let expanded_path: Utf8PathBuf = expand_tilde(&path);
-        let expected_path: Utf8PathBuf = Utf8PathBuf::from_path_buf(home_dir().unwrap())
-            .unwrap()
-            .join("cairo_2_4_3");
-        assert_eq!(expanded_path, expected_path);
     }
 }
