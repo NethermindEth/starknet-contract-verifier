f5682318b3a866330294d7852cb46e59439cccdc
liam <liam@nethermind.io>
2024-10-24 14:17:00 -0400

    fix: read env var

--- crates/cli/src/cli.rs
+++ crates/cli/src/cli.rs
@@ -123,7 +123,10 @@ fn main() -> anyhow::Result<()> {
         custom_internal_api_endpoint_url.is_ok() && custom_public_api_endpoint_url.is_ok();
 
     // Only show local if debug network option is up.
-    let is_debug_network = env::var("DEBUG_NETWORK").is_ok();
+    let is_debug_network = match env::var("DEBUG_NETWORK") {
+        Ok(value) => value.to_lowercase() == "true",
+        Err(_) => false,
+    };
     let network_items = if is_debug_network {
         vec!["Mainnet", "Sepolia", "Integration", "Local"]
     } else {
